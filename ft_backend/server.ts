2f088fe176c768db425ceacd266809adce42e5c5
Armand Mititelu <armitite@fu-r8-p1.s19.be>
2025-06-26 15:44:48 +0200

    Sans gitignore

---
+++ ft_backend/server.ts
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.ts                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 12:50:57 by mokariou          #+#    #+#             */
+/*   Updated: 2025/06/26 15:32:15 by armitite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import fastify from "fastify";
+import path from 'path'
+import fastifyFormbody from '@fastify/formbody';
+import fastifyStatic from '@fastify/static';
+import fastifyCookie from "@fastify/cookie";
+import fastifySession from "@fastify/session";
+import { request } from 'http';
+import { register } from 'module';
+import db from './db/db'
+import { error } from "console";
+import bcrypt from "bcrypt";
+import { REPLServer } from "repl";
+import { LoginRoutes } from './Login/Login'; // This is the 'Module' that I made
+import { DashboardRoutes } from './Dashboard/Dashboard';
+import fastifyCors from "@fastify/cors";
+
+
+// here I create the server
+const server = fastify();
+
+
+declare module "@fastify/session" {
+  interface FastifySessionObject {
+    user?: { id: number; email: string };
+  }
+}
+
+server.register(fastifyCookie);
+server.register(fastifySession, {
+  secret: 'a-very-secret-key-must-be-32-characters',
+  cookie: { secure: false }, // set to true if using HTTPS
+  saveUninitialized: false,
+});
+
+
+server.register(fastifyStatic, {
+	root: path.join(__dirname, "../ft_frontend/dist"),
+	prefix: "/",
+});
+
+server.register(fastifyCors, {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+});
+
+server.register(fastifyFormbody);
+
+LoginRoutes(server);
+DashboardRoutes(server);
+server.listen({ port: 3000 }, (err) => {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	console.log("server is running 🚀");
+});
